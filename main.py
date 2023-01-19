d901b3a4d9426c092a3a736b97d6587bb2047e12
J43fura <mehdi.jaafourag@gmail.com>
2023-01-19 13:22:13 +0100

    Updated

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 
 import requests, os
+#Example: Year:2022, Session:principale, Section:math, Matiere (Subject):math 
+#href="http://www.bacweb.tn/bac/2022/principale/math/math.pdf"
+
+aam_range = 2020,2023 #Year Range
+session = 'principale','controle'
 section = 'technique','math'
 matiere = 'physique','math'
-def sawwr(t):
-    print(f'{sect.upper()[0]}{mat[0:2]}{str(aam)}{sess[0].upper()}{t}')
-#href="bac/2017/principale/technique/math.pdf"
 
+def sawwr(t): #Prints State
+    print(f'{sect} {mat} {aam} {sess}{t}'.upper())
+
+#Create a Folder named MSABB
 dir_path = os.path.join(os.getcwd(), 'MSABB')
 if not os.path.exists(dir_path):
     os.mkdir(dir_path, 0o666)
 
-for sect in section:
+
+for sect in section: #Create a Folder of the Section
     dir_path = os.path.join(os.getcwd(), f'MSABB\\BAC {sect}')
     if not os.path.exists(dir_path):
         os.mkdir(dir_path, 0o666)
-    for mat in matiere:
+
+    for mat in matiere: #Create a Folder of the "Matiere" inside the Section
         dir_path = os.path.join(os.getcwd(), f'MSABB\\BAC {sect}\\{mat}')
         if not os.path.exists(dir_path):
             os.mkdir(dir_path, 0o666)
-        for aam in range(2020,2023):
 
-            for sess in ('principale','controle'):
-                sawwr(' Yetsabb..')
+        for aam in range(*aam_range): #Loop through the years (aam).
+            for sess in (session): #Loop through the sessions
+                sawwr(' Yetsabb..') #Prints about being Downloaded..
                 s=False
                 url = f'http://www.bacweb.tn/bac/{str(aam)}/{sess}/{sect}/{mat}.pdf'
-                print(url)
                 response = requests.get(url)
-                if response.status_code == 200:
-                    sawwr(' Tsabb.')
+                if response.status_code == 200: #Success
+                    sawwr(' Tsabb.\n') #Prints about geting Downloaded.
                     s=True
-                    with open(os.path.join(dir_path, f'{mat.upper()}-{aam}-{sess[0].upper()}.pdf'), 'wb') as pdf:
+                    with open(os.path.join(dir_path, f'{mat.upper()}-{aam}-{sess[0].upper()}.pdf'), 'wb') as pdf: #Create the pdf.
                         for block in response.iter_content(chunk_size=1024):
                             pdf.write(block)
-                if not(s):
-                    sawwr(' Matsabbech.')
+
+                if not(s): #Fail
+                    sawwr(' Matsabbech.') #Prints about not getting downloaded.
