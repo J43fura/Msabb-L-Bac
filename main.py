d822263aea97f09fe2d6ecd2241e572cb77e1c98
J43fura <mehdi.jaafourag@gmail.com>
2023-01-23 21:41:46 +0100

    Done

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 
 import requests, os
 import flet as ft
+import time
 
-aam_range = [2020,2023] #Year Range
-sessions = 'principale','controle'
-matieres_fix = 'physique','math','philo','francais','arabe','anglais'
+
+matieres_fix = 'math','philo','francais','arabe','anglais'
 section_matieres = {
-    'math' : ['info','svt',*matieres_fix],
-    'sciences_ex' : ['info','svt',*matieres_fix],
+    'math' : ['info','svt','physique',*matieres_fix],
+    'sciences_ex' : ['info','svt','physique',*matieres_fix],
     'economie_gestion' : ['economie','gestion','his_geo','info',*matieres_fix],
-    'technique' : ['technique','info',*matieres_fix],
+    'technique' : ['technique','info','physique',*matieres_fix],
     'lettre' : ['pensee','his_geo','info','svt',*matieres_fix],
-    'sport' : ['sport','info','svt',*matieres_fix],
-    'informatique' : ['bd','algorithme',*matieres_fix]
+    'sport' : ['sport','info','svt','physique',*matieres_fix],
+    'informatique' : ['bd','algorithme','physique',*matieres_fix]
     }
-matieres=[]
-
+matieres_in=[]
 options = 'theatre','artistique','musique','italien','portugais','turque','chinois','russe','espagnol','allemand'
+year_range = [2020,2023] #Year Range
 
+def create_folder(t):
+    global dir_path
+    dir_path = os.path.join(os.getcwd(), t)
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path, 0o666)
 
-#FLUT
+#GUI with FLET
 def main(page:ft.Page):
     page.theme_mode=ft.ThemeMode.DARK
     page.window_height=700
@@ -27,161 +32,151 @@ def main(page:ft.Page):
     page.title="Msabb l BAC"
     page.scroll=True
 
-    #REQUESTS
+    #Download
     def Download(e):
         #Example: Year:2022, sessions:principale, Section:math, Matiere (Subject):math 
-        #href="http://www.bacweb.tn/bac/2022/principale/math/math.pdf"
-        print(section, matieres,aam_range)
-        matiere=[]
+        #http://www.bacweb.tn/bac/2022/principale/math/math.pdf
         sessions=[]
-        for mat in matieres:
+        matieres=[]
+        for mat in matieres_in:
             if mat.value:
-                matiere.append(mat.label)
-                print(opt,opt.value)
-        if opt.value != None:
-            matiere.append(opt.value)
-        print(matiere)
-        for s in SessionP,SessionC:
-            if s.value != None:
-                sessions.append(s.label)
-        print(sessions)
-        print(aam_range)
-
-        def sawwr(t): #Prints State
-            print(f'{sect} {mat} {aam} {sess}{t}'.upper())
-
+                matieres.append(mat.label)
+                if correction_in.value:
+                    matieres.append( f"{mat.label}_c")
+        if option_in.value != None:
+            matieres.append(option_in.value)
+        for session_in in session_princiaple_in,session_controle_in:
+            if session_in.value == True:
+                sessions.append((session_in.label).lower())
         #Create a Folder named MSABB
-        dir_path = os.path.join(os.getcwd(), 'MSABB')
-        if not os.path.exists(dir_path):
-            os.mkdir(dir_path, 0o666)
-
-
+        create_folder('MSABB')
+        progress_counter=0
+        progress_bar_column.visible = True
+        start_time = time.time()
+        iter_number = len(section)*len(matieres)*(year_range[1]-year_range[0])*len(sessions)
         for sect in section: #Create a Folder of the Section
-            dir_path = os.path.join(os.getcwd(), f'MSABB\\BAC {sect}')
-            if not os.path.exists(dir_path):
-                os.mkdir(dir_path, 0o666)
+            create_folder(f'MSABB\\BAC {sect}')
+            for mat in matieres: #Create a Folder of the "Matiere" inside the Section
+                create_folder(f'MSABB\\BAC {sect}\\{mat}')
+                for year in range(*year_range): #Loop through the years (year).
+                    for sess in sessions: #Loop through the sessions
+                        success=False
+                        #matieres communs : sections communs, default: math
+                        #philo,arabe: technique, math, science, eco, info.
+                        #francais,anglais: math, science, eco, info.
+                        #info: technique, math, science.
+                        #option: all.
+                        url = f'http://www.bacweb.tn/bac/{str(year)}/{sess}/math/{mat}.pdf' if \
+                        (mat in ["philo","arabe"] and sect in ["technique","economie_gestion","sciences_ex","informatique"]) or \
+                        (mat in ["francais","anglais"] and sect in ["economie_gestion","sciences_ex","informatique"]) or \
+                        (mat =="info" and sect in ["technique","sciences_ex"]) or \
+                        (mat == option_in.value) \
+                        else f'http://www.bacweb.tn/bac/{str(year)}/{sess}/{sect}/{mat}.pdf'
+                        progress_text.value  = f"Downloading: {url}"
+                        page.update()
 
-            for mat in matiere: #Create a Folder of the "Matiere" inside the Section
-                dir_path = os.path.join(os.getcwd(), f'MSABB\\BAC {sect}\\{mat}')
-                if not os.path.exists(dir_path):
-                    os.mkdir(dir_path, 0o666)
-
-                for aam in range(*aam_range): #Loop through the years (aam).
-                    for sess in (sessions): #Loop through the sessions
-                        sawwr(' Yetsabb..') #Prints about being Downloaded..
-                        s=False
-                        url = f'http://www.bacweb.tn/bac/{str(aam)}/{sess}/{sect}/{mat}.pdf'
                         response = requests.get(url)
                         if response.status_code == 200: #Success
-                            sawwr(' Tsabb.\n') #Prints about geting Downloaded.
-                            s=True
-                            with open(os.path.join(dir_path, f'{mat.upper()}-{aam}-{sess[0].upper()}.pdf'), 'wb') as pdf: #Create the pdf.
+                            success=True
+                            with open(os.path.join(dir_path, f'{mat.upper()}-{year}-{sess[0].upper()}.pdf'), 'wb') as pdf: #Create the pdf.
                                 for block in response.iter_content(chunk_size=1024):
                                     pdf.write(block)
+                            progress_text.value=f"Downloaded: {url}"
+                        if not(success): #Fail
+                            progress_text.value=f"Doesnt exist: {url}"
 
-                        if not(s): #Fail
-                            sawwr(' Matsabbech.') #Prints about not getting downloaded.
+                        progress_counter+=1
+                        progress_bar.value = progress_counter / iter_number
+                        page.update()
+        
+        end_time = time.time()
+        progress_text.value = f"Downloaded {len(ts)} files in {int(end_time-start_time)} seconds."
+        page.update()
 
 
-
+    #Section
     page.add(ft.Text("Section:"))
-    def DownloadTest(e):
-        print(section, matieres,aam_range)
-        matiere=[]
-        sessions=[]
-        for mat in matieres:
-            if mat.value:
-                matiere.append(mat.label)
-                print(opt,opt.value)
-        if opt.value != None:
-            matiere.append(opt.value)
-        print(matiere)
-        for s in SessionP,SessionC:
-            if s.value != None:
-                sessions.append(s.label)
-        print(sessions)
-        print(aam_range)
 
-    
+    #Download Button
     download_button = ft.ElevatedButton("Download",icon="DOWNLOAD",on_click=Download,visible=False)
     page.add(download_button)
 
 
-
-
-    #MATIERES DU SECTION
+    #Matieres du section
     def matieres_update(e):
-        global matieres
+        global matieres_in
         global section 
-        section = dsc.value
+        section = [section_in.value]
         page.remove(download_button)
-        if (matieres is not(None)):
-            
-            for matiere in matieres:
-                page.remove(matiere)
-                print("-" ,matiere.label," ", matiere)
-                
+        if (matieres_in is not(None)):
+            for matiere_in in matieres_in:
+                page.remove(matiere_in)
             else:
-                matieres = []
-                for matiere in section_matieres[dsc.value]:
+                matieres_in = []
+                for matiere in section_matieres[section_in.value]:
                     matiere = ft.Checkbox(label=matiere, value=True)
                     page.add(matiere)    
-                    matieres.append(matiere)
-                    print("+" ,matiere.label," ", matiere)
+                    matieres_in.append(matiere)
                 page.add(download_button)
                 download_button.visible = True
 
         page.update()
 
-    dsc = ft.Dropdown(
+    section_in = ft.Dropdown(
         options=[ft.dropdown.Option(section) for section in section_matieres.keys()],on_change=matieres_update,
     )
-    page.add(dsc)
+    page.add(section_in)
     
 
 
-    #sessions
-    page.add(ft.Text("sessions:"))
-    global SessionP,SessionC
-    SessionP,SessionC = ft.Checkbox(label="Principale", value=True),ft.Checkbox(label="Controle", value=True)
-    page.add(SessionP,SessionC)
+    #SESSIONS
+    global session_princiaple_in,session_controle_in
+    session_princiaple_in,session_controle_in = ft.Checkbox(label="Principale", value=True),ft.Checkbox(label="Controle", value=True)
+    page.add(ft.Text("Sessions:"),session_princiaple_in,session_controle_in)
 
-    def slider_changed(e):
-        t_ad.value = e.control.value
-        aam_range[0]= int(t_af.value)
+    #CORRECTION 
+    global correction_in
+    correction_in = ft.Switch(label="Correction", value=True)
+    page.add(ft.Text("Correction:"),correction_in)
+
+    #Year Range
+    def slider_changedMin(e):
+        year_min_in.value = e.control.value
+        year_range[0]= int(year_min_in.value)
         page.update()
 
-    t_ad = ft.Text()
+    year_min_in = ft.Text()
     page.add(
-        ft.Text("année de début:"),
-        ft.Slider(min=1994, max=2030, divisions=(36), label="{value}", on_change=slider_changed), t_ad)
+        ft.Text("De l'année:"),
+        ft.Slider(min=2009, max=2030, divisions=(21), label="{value}", on_change=slider_changedMin), year_min_in)
 
-    def slider_changed(e):
-        t_af.value = e.control.value
-        aam_range[1]= int(t_af.value)
+    def slider_changedMax(e):
+        year_max_in.value = e.control.value
+        year_range[1]= int(year_max_in.value)+1
         page.update()
 
-    t_af = ft.Text()
+    year_max_in = ft.Text()
     page.add(
-        ft.Text("dernière année:"),
-        ft.Slider(min=1994, max=2030, divisions=(36), label="{value}", on_change=slider_changed), t_af)
+        ft.Text("Jusqu'a l'année:"),
+        ft.Slider(min=2009, max=2030, divisions=(21), label="{value}", on_change=slider_changedMax), year_max_in)
 
-
+    #OPTION
     page.add(ft.Text("Option:"))
-
-    global opt 
-    opt = ft.Dropdown(
-        options=[ft.dropdown.Option(opt) for opt in options])
-    page.add(opt)
+    global option_in 
+    option_in = ft.Dropdown(
+        options=[ft.dropdown.Option(option) for option in options])
+    page.add(option_in)
 
 
     #matieres
-
-
-
-
-
     page.add(ft.Text("Matieres:"))
+    global progress_bar
+    global progress_text
+    progress_text=ft.Text()
+    progress_bar = ft.ProgressBar()
+    progress_bar_column = ft.Column([ progress_text, progress_bar],visible=False)
+    page.add(progress_bar_column)
+
 
 
 ft.app(target=main)
